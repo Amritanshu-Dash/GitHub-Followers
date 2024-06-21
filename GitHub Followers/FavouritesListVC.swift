2d0e013353f33ccf4f9cb468fef96a81762e2017
Amritanshu Dash <amritanshudash6677@gmail.com>
2024-06-21 16:59:26 +0530

    Implemeted the tab view and navigation views for favorites and search screens.

---
+++ GitHub Followers/FavouritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavouritesListVC.swift
+//  GitHub Followers
+//
+//  Created by Amritanshu Dash on 21/06/24.
+//
+
+import UIKit
+
+class FavouritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemRed
+    }
+
+}
---
+++ GitHub Followers/SearchVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchVC.swift
+//  GitHub Followers
+//
+//  Created by Amritanshu Dash on 21/06/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+
+
+}
